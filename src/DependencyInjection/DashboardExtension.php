b7d5933b07e600b2f7265b7c813745f8657cadbc
David Dutas <david.dutas@gmail.com>
2021-12-04 23:55:55 +0100

    Adapt to bootstrap5

--- src/DependencyInjection/DashboardExtension.php
+++ src/DependencyInjection/DashboardExtension.php
@@ -2,12 +2,13 @@
 
 namespace ICS\DashboardBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
 
-class DashboardExtension extends Extension
+class DashboardExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -16,10 +17,26 @@ class DashboardExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config/'));
         $loader->load('services.yaml');
+        
 
         $configuration = new Configuration();
         $configs = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter('dashboard', $configs);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config/'));
+        $bundles = $container->getParameter('kernel.bundles');
+        
+        if(isset($bundles['DoctrineBundle']))
+        {
+            $loader->load('doctrine.yaml');
+        }
     }
 }
